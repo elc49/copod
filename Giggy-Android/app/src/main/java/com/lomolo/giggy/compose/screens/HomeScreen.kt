9626f9da01b127082ab6c101017080f072893f5d
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-05-27 11:04:07 +0300

    feat: landing screen

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/HomeScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/HomeScreen.kt
@@ -1,9 +1,25 @@
 package com.lomolo.giggy.compose.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lomolo.giggy.R
 import com.lomolo.giggy.compose.navigation.Navigation
+import com.lomolo.giggy.ui.theme.GiggyTheme
 
 object HomeScreenDestination: Navigation {
     override val route = "home"
@@ -14,5 +30,57 @@ object HomeScreenDestination: Navigation {
 fun HomeScreen(
     modifier: Modifier = Modifier
 ) {
-    Text(text = "Welcome")
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.weight(2f)
+        ) {
+            Text(
+                text = "We farm",
+                style = MaterialTheme.typography.displayMedium,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Row(
+            modifier = Modifier.weight(2f)
+        ) {
+            Column {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = stringResource(R.string.lear_connect_share),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.weight(.5f, false)
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                shape = MaterialTheme.shapes.extraSmall
+            ) {
+                Text(
+                    text = "Get Started",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    GiggyTheme {
+        HomeScreen()
+    }
 }
