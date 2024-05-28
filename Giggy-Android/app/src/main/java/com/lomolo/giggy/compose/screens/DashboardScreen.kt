55c5e2d43a3ff5c1075523735c02aab45c26df63
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-05-28 11:26:41 +0300

    chore: cleanup

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/DashboardScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/DashboardScreen.kt
@@ -3,41 +3,22 @@ package com.lomolo.giggy.compose.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.twotone.LocationOn
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedSuggestionChip
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.lomolo.giggy.R
+import com.lomolo.giggy.compose.PostCard
 import com.lomolo.giggy.ui.theme.GiggyTheme
 import com.lomolo.giggy.ui.theme.inverseOnSurfaceLight
 
@@ -65,137 +46,11 @@ fun DashboardScreen(
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun ContentCard(
-    modifier: Modifier = Modifier,
-    text: String,
-    images: List<String>
-) {
-    val context = LocalContext.current
-
-    Column {
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ){
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                AsyncImage(
-                    model = ImageRequest.Builder(context)
-                        .data(testPost.img)
-                        .crossfade(true)
-                        .build(),
-                    contentScale = ContentScale.Crop,
-                    placeholder = painterResource(id = R.drawable.loading_img),
-                    modifier = Modifier
-                        .size(40.dp)
-                        .clip(CircleShape),
-                    contentDescription = null
-                )
-                Text(
-                    text = "029ulskhfl",
-                    style = MaterialTheme.typography.bodySmall,
-                    fontWeight = FontWeight.ExtraBold,
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
-            Text(
-                text = "1h ago",
-                style = MaterialTheme.typography.bodySmall,
-                fontWeight = FontWeight.ExtraBold,
-                color = MaterialTheme.colorScheme.primary
-            )
-        }
-        Text(
-            text = text,
-            style = MaterialTheme.typography.bodyMedium
-        )
-        if (images.isNotEmpty()) {
-            Card(
-                modifier = Modifier
-                    .height(200.dp)
-                    .padding(top = 4.dp, bottom = 4.dp)
-                    .fillMaxWidth(),
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.background,
-                )
-            ) {
-                Box(
-                    Modifier
-                        .fillMaxSize()
-                        .padding(top = 4.dp)) {
-                    images.map {
-                        AsyncImage(
-                            model = ImageRequest.Builder(context)
-                                .data(it)
-                                .crossfade(true)
-                                .build(),
-                            placeholder = painterResource(id = R.drawable.loading_img),
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .clip(MaterialTheme.shapes.extraSmall),
-                            contentDescription = null
-                        )
-                    }
-
-                }
-            }
-        }
-        Box {
-            // TODO trim extra lengthy tags
-            FlowRow {
-                testPost.tags.map {
-                    ElevatedSuggestionChip(
-                        shape = MaterialTheme.shapes.extraSmall,
-                        onClick = { /*TODO*/ },
-                        modifier = Modifier
-                            .padding(top = 4.dp, bottom = 4.dp, end = 4.dp)
-                            .height(20.dp),
-                        colors = SuggestionChipDefaults.elevatedSuggestionChipColors(
-                            containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        ),
-                        label = {
-                            // TODO trim extra lengthy wording
-                            Text(
-                                it,
-                                textAlign = TextAlign.Center,
-                                style = MaterialTheme.typography.labelMedium,
-                                fontWeight = FontWeight.ExtraBold
-                            )
-                        }
-                    )
-                }
-            }
-        }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            Icon(
-                Icons.TwoTone.LocationOn,
-                modifier = Modifier.size(16.dp),
-                tint = MaterialTheme.colorScheme.inversePrimary,
-                contentDescription = null
-            )
-            Text(
-                text = "Ololua road, Ololua Ward, Kajiado - North",
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.inversePrimary
-            )
-        }
-    }
-}
-
 @Composable
 internal fun Content(
     modifier: Modifier = Modifier
 ) {
-    ContentCard(
+    PostCard(
         text = testPost.post,
         images = listOf(
             testPost.img
