f744b41ed5ef97f9e33f14fb98588af765f86485
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-05-28 23:34:32 +0300

    chore(ui): display dummy data

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/FarmStoreProductScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/FarmStoreProductScreen.kt
@@ -1,6 +1,7 @@
 package com.lomolo.giggy.compose.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,10 +9,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,6 +40,50 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.lomolo.giggy.R
 import com.lomolo.giggy.ui.theme.GiggyTheme
+
+data class ProductData(
+    val img: String,
+    val unit: String,
+    val price: Int,
+    val volume: Int,
+)
+
+data class OrderData(
+    val name: String,
+    val volume: Int,
+    val product_id: String = "",
+    val amount: Int,
+)
+
+data class PaymentData(
+    val phone: String,
+    val amount: Int,
+    val status: String,
+)
+
+val testProductsData = listOf(
+    ProductData(
+        "https://storage.googleapis.com/giggy-cloud-storage/guava.jpeg",
+        "kg",
+        124,
+        50,
+    )
+)
+val testOrdersData = listOf(
+    OrderData(
+        "Guava",
+        5,
+        "product_id",
+        620,
+    )
+)
+val testPaymentsData = listOf(
+    PaymentData(
+        "+254739218799",
+        620,
+        "paid",
+    )
+)
 
 @ExperimentalMaterial3Api
 @Composable
@@ -109,6 +159,114 @@ fun FarmStoreProductScreen(
                    }
                )
            }
+        }
+        when(state) {
+            1 -> LazyColumn {
+                item {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                    ) {
+                        Text(
+                            "Product",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            "Volume",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            "Paid",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+                items(testOrdersData) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            it.name,
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            "${it.volume}",
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            "${it.amount}",
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                }
+            }
+            2 -> LazyColumn {
+                item {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                    ) {
+                        Text(
+                            "Customer",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            "Amount",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            "Status",
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+                items(testPaymentsData) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        Text(
+                            it.phone,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            modifier = Modifier.width(100.dp),
+                        )
+                        Text(
+                            "${it.amount}",
+                            textAlign = TextAlign.Center,
+                        )
+                        SuggestionChip(
+                            onClick = { /*TODO*/ },
+                            colors = SuggestionChipDefaults.suggestionChipColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                labelColor = MaterialTheme.colorScheme.background,
+                            ),
+                            label = {
+                                Text(it.status, fontWeight = FontWeight.ExtraBold)
+                            },
+                        )
+                    }
+                }
+            }
         }
     }
 }
