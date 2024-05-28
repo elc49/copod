fe0bc9b22c53df8e83c6ca53270c99a1b33a5f94
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-05-28 18:45:25 +0300

    chore(ui): cleanup

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/FarmStoreProductScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/FarmStoreProductScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.PrimaryScrollableTabRow
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -84,7 +84,7 @@ fun FarmStoreProductScreen(
                 fontWeight = FontWeight.ExtraBold
             )
         }
-        PrimaryScrollableTabRow(
+        PrimaryTabRow(
             modifier = Modifier.fillMaxWidth(),
             selectedTabIndex = state
         ) {
