6223ba362c251a4c0e7b4487cc8cd536c9cf5d4f
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-05-28 15:03:36 +0300

    chore(ui): cleanup

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/MarketScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/MarketScreen.kt
@@ -1,33 +1,13 @@
 package com.lomolo.giggy.compose.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.lomolo.giggy.R
 import com.lomolo.giggy.ui.theme.GiggyTheme
-import com.lomolo.giggy.ui.theme.surfaceDimLight
 
 data class Product(
     val name: String,
@@ -58,82 +38,7 @@ fun MarketScreen(
     }
 }
 
-@Composable
-internal fun MarketCard(
-    modifier: Modifier = Modifier,
-    data: Product
-) {
-    OutlinedCard(
-        modifier = Modifier
-            .height(180.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Box(Modifier.weight(1f)) {
-                AsyncImage(
-                    model = ImageRequest.Builder(LocalContext.current)
-                        .data(data.img)
-                        .crossfade(true)
-                        .build(),
-                    contentDescription = null,
-                    placeholder = painterResource(id = R.drawable.loading_img),
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(MaterialTheme.shapes.extraSmall)
-                )
-            }
-            Box(
-                Modifier.weight(1f),
-            ) {
-                Box(
-                    Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(4.dp)
-                    ) {
-                        Text(
-                            text = data.name,
-                            style = MaterialTheme.typography.titleMedium,
-                            textAlign = TextAlign.Center,
-                            fontWeight = FontWeight.Bold,
-                        )
-                        Text(
-                            text = "KES ${data.price} per ${data.metric}",
-                            style = MaterialTheme.typography.bodyLarge,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                }
-                Box(
-                    Modifier
-                        .align(Alignment.BottomCenter)
-                        .padding(8.dp)) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Icon(
-                            painterResource(id = R.drawable.farmer),
-                            modifier = Modifier
-                                .size(20.dp),
-                            contentDescription = null
-                        )
-                        Text(
-                            data.farm,
-                            style = MaterialTheme.typography.labelSmall,
-                            textAlign = TextAlign.Center,
-                            color = surfaceDimLight
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
+
 
 @Preview
 @Composable
