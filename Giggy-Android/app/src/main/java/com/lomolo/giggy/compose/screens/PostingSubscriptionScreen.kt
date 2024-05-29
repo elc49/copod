b5240ae04b208a60b851168c5eeb2ac2e6276795
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-05-29 13:42:27 +0300

    feat: posting subscription screen

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/PostingSubscriptionScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/PostingSubscriptionScreen.kt
@@ -1,14 +1,112 @@
 package com.lomolo.giggy.compose.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Check
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lomolo.giggy.R
 import com.lomolo.giggy.ui.theme.GiggyTheme
 
 @Composable
 fun PostingSubscriptionScreen(
     modifier: Modifier = Modifier
-) {}
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = stringResource(R.string.buy_posting_rights),
+            style = MaterialTheme.typography.displayLarge,
+            textAlign = TextAlign.Center,
+        )
+        Box {
+            Column {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.TwoTone.Check,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        stringResource(id = R.string.one_time_purchase)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.TwoTone.Check,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        stringResource(R.string.share_voice)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.TwoTone.Check,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        stringResource(R.string.announce_events)
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.TwoTone.Check,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Text(
+                        stringResource(R.string.advertise)
+                    )
+                }
+            }
+        }
+        Box(Modifier.padding(8.dp)) {
+            Button(
+                contentPadding = PaddingValues(12.dp),
+                onClick = { /*TODO*/ },
+                shape = MaterialTheme.shapes.extraSmall,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+               Text(
+                   "KES ${1800}",
+                   fontWeight = FontWeight.Bold,
+                   style = MaterialTheme.typography.titleMedium,
+               )
+            }
+        }
+    }
+}
 
 @Preview
 @Composable
