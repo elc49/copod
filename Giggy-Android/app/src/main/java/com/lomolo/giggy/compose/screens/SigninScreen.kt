3459683e21d39110929d7acc98e11c73e6b65ce7
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-05-31 00:50:53 +0300

    chore: cleanup

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/SigninScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/SigninScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -75,6 +76,7 @@ fun SignInScreen(
                                 .decoderFactory(SvgDecoder.Factory())
                                 .build(),
                             contentScale = ContentScale.Crop,
+                            placeholder = painterResource(id = R.drawable.loading_img),
                             modifier = Modifier
                                 .size(32.dp),
                             contentDescription = null
@@ -99,7 +101,7 @@ fun SignInScreen(
                 onClick = { /*TODO*/ }
             ) {
                Text(
-                   text = "Sign In",
+                   text = stringResource(R.string.sign_in),
                    style = MaterialTheme.typography.titleMedium,
                    fontWeight = FontWeight.Bold
                )
