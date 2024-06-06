da74d448ef1850a9db7128a85b6c7434f3b827f3
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-06-06 10:27:13 +0300

    test: signin controller mods

--- Giggy-Server/tests/signin_controller_test.go
+++ Giggy-Server/tests/signin_controller_test.go
@@ -4,19 +4,37 @@ import (
 	"context"
 	"testing"
 
+	"github.com/elc49/giggy-monorepo/Giggy-Server/graph/model"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSigninController(t *testing.T) {
+	var u *model.User
 	signinC := signinController()
 	ctx := context.Background()
+	phone := "254791215745"
 
 	t.Run("create_user_by_phone", func(t *testing.T) {
-		phone := "254791215745"
-		user, err := signinC.CreateUserByPhone(ctx, phone)
+		var err error
+		u, err = signinC.CreateUserByPhone(ctx, phone)
 
 		assert.Nil(t, err)
-		assert.NotNil(t, user)
-		assert.Equal(t, user.Phone, phone)
+		assert.Equal(t, u.Phone, phone, "phones should be equal")
+	})
+
+	t.Run("get_user_by_phone", func(t *testing.T) {
+		var err error
+		u, err = signinC.GetUserByPhone(ctx, phone)
+
+		assert.Nil(t, err)
+		assert.NotNil(t, u, "should have user object")
+	})
+
+	t.Run("get_user_by_id", func(t *testing.T) {
+		var err error
+		u, err = signinC.GetUserByID(ctx, u.ID)
+
+		assert.Nil(t, err)
+		assert.NotNil(t, u, "should have user object")
 	})
 }
