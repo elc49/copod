855bea08f37ca2f6283d24880e6d509024eb3312
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-06-10 22:20:13 +0300

    gps: use default ip location in post location

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/viewmodels/PostingViewModel.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/viewmodels/PostingViewModel.kt
@@ -1,12 +1,16 @@
 package com.lomolo.giggy.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.lomolo.giggy.model.DeviceDetails
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class PostingViewModel: ViewModel() {
+class PostingViewModel(
+    private val mainViewModel: MainViewModel,
+): ViewModel() {
     private val _postInput = MutableStateFlow(Posting())
     val postingUiState: StateFlow<Posting> = _postInput.asStateFlow()
 
@@ -16,7 +20,20 @@ class PostingViewModel: ViewModel() {
         }
     }
 
+    // use ip gps if we don't have device gps permissions
+    private fun postGps(deviceDetails: DeviceDetails): LatLng {
+        if (deviceDetails.deviceGps.latitude == 0.0 && deviceDetails.deviceGps.longitude == 0.0) {
+            // use ip gps
+            val gps = deviceDetails.ipGps.split(",")
+            return LatLng(gps[0].toDouble(), gps[1].toDouble())
+        }
+        return deviceDetails.deviceGps
+    }
+
     fun savePost(cb: () -> Unit = {}) {
+        _postInput.update {
+            it.copy(location = postGps(mainViewModel.deviceDetailsState.value))
+        }
         if (_postInput.value.text.isNotBlank()) {
             cb()
         }
@@ -28,5 +45,6 @@ class PostingViewModel: ViewModel() {
 }
 
 data class Posting(
-    val text: String = ""
+    val text: String = "",
+    val location: LatLng = LatLng(0.0, 0.0),
 )
