cd26faa1690c5a99882fd50f3e412bcb6f7bb19c
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-06-12 12:15:22 +0300

    ui: return upload ui

--- Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/CreatePostScreen.kt
+++ Giggy-Android/app/src/main/java/com/lomolo/giggy/compose/screens/CreatePostScreen.kt
@@ -8,12 +8,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,11 +23,17 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.lomolo.giggy.R
 import com.lomolo.giggy.compose.navigation.Navigation
 import com.lomolo.giggy.ui.theme.GiggyTheme
@@ -103,7 +111,6 @@ fun CreatePostScreen(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(4.dp)
                 ) {
-                    /*
                     AsyncImage(
                         model = ImageRequest.Builder(LocalContext.current)
                             .data("https://storage.googleapis.com/giggy-cloud-storage/download.jpeg")
@@ -126,7 +133,6 @@ fun CreatePostScreen(
                             contentDescription = null
                         )
                     }
-                     */
                 }
             }
             Button(
