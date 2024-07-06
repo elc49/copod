f683b615293054abc92b7d0fc5a0d55747b442f1
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-07-06 18:51:54 +0300

    paystack: setup middleware and handlers

--- Giggy-Server/handlers/webhook/paystack.go
+++ Giggy-Server/handlers/webhook/paystack.go
@@ -1,9 +1,32 @@
 package paystack
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/elc49/giggy-monorepo/Giggy-Server/graph/model"
+	"github.com/sirupsen/logrus"
+)
 
 func Paystack() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		paystackRes := &model.ChargeMpesaPhoneCallbackRes{}
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logrus.WithError(err).Errorf("paystack webhook: io.ReadAll(r.Body)")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		mErr := json.Unmarshal(body, &paystackRes)
+		if mErr != nil {
+			logrus.WithError(mErr).Error("paystack webhook: json.Unmarshal()")
+			http.Error(w, mErr.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		logrus.Infoln(paystackRes)
 		w.WriteHeader(http.StatusOK)
 	})
 }
--- Giggy-Server/middleware/paystack.go
+++ Giggy-Server/middleware/paystack.go
@@ -1,9 +1,35 @@
 package middleware
 
-import "net/http"
+import (
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/elc49/giggy-monorepo/Giggy-Server/config"
+	"github.com/sirupsen/logrus"
+)
 
 func Paystack(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		paystackSignature := r.Header.Get("x-paystack-signature")
+
+		hash := hmac.New(sha512.New, []byte(config.Configuration.Paystack.SecretKey))
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			logrus.WithError(err).Errorf("middleware: io.ReadAll(r.Body)")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		hash.Write(body)
+		expectedHmac := hex.EncodeToString(hash.Sum(nil))
+		if expectedHmac != paystackSignature {
+			http.Error(w, errors.New("paystack: invalid signature").Error(), http.StatusUnauthorized)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
