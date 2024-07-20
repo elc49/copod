2d31cfa9b300fe763f232548c30264c1c06d8315
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-07-20 23:16:27 +0300

    test: persist poster location

--- Giggy-Server/tests/post_controller_test.go
+++ Giggy-Server/tests/post_controller_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/elc49/giggy-monorepo/Giggy-Server/graph/model"
 	"github.com/elc49/giggy-monorepo/Giggy-Server/postgres/db"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,12 +22,11 @@ func TestPostController(t *testing.T) {
 	}()
 
 	t.Run("create_post", func(t *testing.T) {
-		p, err := postC.CreatePost(ctx, db.CreatePostParams{
+		p, err := postC.CreatePost(ctx, user.ID, model.NewPostInput{
 			Text:     "Shiny evening here at the farm",
 			Image:    avatar,
 			Tags:     []string{"animal feed", "farm inputs"},
-			UserID:   user.ID,
-			Location: fmt.Sprintf("SRID=4326;POINT(%.8f %.8f)", 36.1289, -1.2743),
+			Location: &model.GpsInput{Lng: 36.1289, Lat: -1.2743},
 		})
 		assert.Nil(t, err)
 		assert.NotNil(t, p)
