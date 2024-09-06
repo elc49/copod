2cf90441979b22716acc7de1b61fa51264134aba
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-09-06 17:16:37 +0300

    chore: prepare deploy

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.lomolo.copod"
         minSdk = 28
         targetSdk = 34
-        versionCode = 10
-        versionName = "2.2.4"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
