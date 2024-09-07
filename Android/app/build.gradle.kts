8b1c85066de88371039c022e48ae272238fd5185
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-09-07 13:04:30 +0300

    release: v1.0.4

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.lomolo.copod"
         minSdk = 28
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
