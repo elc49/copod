2f6b2c90dd0a83dccc22bf7e70cb19507bfa7722
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-09-25 10:52:26 +0300

    ui: redo farm store ui

--- Android/app/src/main/java/com/lomolo/copod/compose/navigation/FarmGraph.kt
+++ Android/app/src/main/java/com/lomolo/copod/compose/navigation/FarmGraph.kt
@@ -1,27 +1,14 @@
 package com.lomolo.copod.compose.navigation
 
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.twotone.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
-import com.lomolo.copod.R
 import com.lomolo.copod.compose.screens.CreateFarmMarketDestination
 import com.lomolo.copod.compose.screens.CreateFarmMarketScreen
 import com.lomolo.copod.compose.screens.FarmSettingsScreen
@@ -54,33 +41,12 @@ fun NavGraphBuilder.addFarmGraph(
                 type = NavType.StringType
             })
         ) {
-            Scaffold(contentWindowInsets = WindowInsets(0, 0, 0, 0),
-                snackbarHost = { copodSnackbarHost(snackbarHostState) },
-                topBar = {
-                    TopAppBar(windowInsets = WindowInsets(0, 0, 0, 0),
-                        title = {},
-                        navigationIcon = {
-                            IconButton(onClick = {
-                                navHostController.popBackStack()
-                            }) {
-                                Icon(
-                                    Icons.AutoMirrored.TwoTone.ArrowBack,
-                                    contentDescription = stringResource(id = R.string.go_back),
-                                )
-                            }
-                        })
-                }) {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(it)
-                ) {
-                    FarmStoreScreen(
-                        deviceDetails = deviceDetails,
-                        navHostController = navHostController,
-                    )
-                }
-            }
+            FarmStoreScreen(
+                deviceDetails = deviceDetails,
+                copodSnackbarHost = copodSnackbarHost,
+                snackbarHostState = snackbarHostState,
+                navHostController = navHostController,
+            )
         }
         composable(
             route = FarmSettingsScreenDestination.routeWithArgs,
